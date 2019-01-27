3e426b9e51ec6c245fdcb400c2a20ac261f881fd
magicodestudio <41899416+magicodestudio@users.noreply.github.com>
2019-01-27 11:47:02 +0200

    Update home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { listing } from '../pages/listing';
+import { listing } from '../pages/page-listing';
 
 @Component({
   selector: 'page-home',
