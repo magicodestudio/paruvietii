c4276cee25a9c06d3c5eb0915759e808117b34f2
magicodestudio <41899416+magicodestudio@users.noreply.github.com>
2019-01-27 11:25:43 +0200

    pagina noua de listing

---
+++ src/pages/listing/listing.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ListingPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-listing',
+  templateUrl: 'listing.html',
+})
+export class ListingPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ListingPage');
+    
+    let v = document.querySelectorAll('#vopseluri option');
+    let cv = document.querySelector('#cauta-vopsea');
+    
+    cv.addEventListener('click', nuSelecta(e));
+    function nuSelecta(e){
+        e.preventDefault;
+    }
+    
+    cv.querySelector('input').addEventListener('keyup', cautareVopseluri);
+    function cautareVopseluri(){
+        let textCautat = cv.querySelector('input').value;
+        for (let i = 1; i < v.length; i++){
+            if (v[i].innerHTML.indexOf(textCautat) == -1){
+                v[i].style.display = 'none';
+            } else {
+                v[i].style.display = 'block';
+            }
+        }
+    }
+    
+  }
+
+}
